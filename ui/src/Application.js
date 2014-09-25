ca61eafbba9eaa8ad6540afa783637df18a5d21d
Priit Kallas <kallaspriit@gmail.com>
2014-09-25 22:11:15 +0300

    Added self-logging.

--- ui/src/Application.js
+++ ui/src/Application.js
@@ -1,12 +1,21 @@
 define([
-], function() {
+	'logviking/Logger',
+	'logviking/ConsoleLog',
+	'logviking/SocketLog',
+], function(logger, ConsoleLog, SocketLog) {
+	var log = logger.get('Application');
 
 	var Application = function() {
-
+		this._setupLogger();
 	};
 
 	Application.prototype.bootstrap = function() {
-		console.log('bootstrap');
+		log.info('bootstrapping');
+	};
+
+	Application.prototype._setupLogger = function() {
+		logger.addReporter(new ConsoleLog());
+		logger.addReporter(new SocketLog('192.168.1.69', 2222));
 	};
 
 	return Application;
